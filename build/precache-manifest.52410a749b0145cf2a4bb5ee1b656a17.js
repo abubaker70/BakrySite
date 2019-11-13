cf03e32723913bb7c014ab1224575a304772ee58
abubaker70 <57699491+abubaker70@users.noreply.github.com>
2019-11-13 10:54:21 +0300

    Add files via upload

---
+++ build/precache-manifest.52410a749b0145cf2a4bb5ee1b656a17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1a8b5cd96d45510bdb387853251878",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e1e6677dc4833808476",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "56dcac3d0cf24346ba54",
+    "url": "/static/js/2.0e72d2bb.chunk.js"
+  },
+  {
+    "revision": "4e1e6677dc4833808476",
+    "url": "/static/js/main.41626f62.chunk.js"
+  },
+  {
+    "revision": "f84b1829c4885fcf84e4",
+    "url": "/static/js/runtime-main.e9e0ccad.js"
+  }
+]);
